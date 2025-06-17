6e9ae413c35112bbf0e907c4f511ad6d6f758c8f
Moisesmp75 <moreperez501@hotmail.com>
2025-06-17 13:32:23 -0500

    add safe string

---
+++ src/shared/domain/models/valueobjects/SafeString.ts
@@ -0,0 +1,30 @@
+
+class SafeString {
+  private value: string;
+
+  private static dangerousCharactersPattern = /['"`;<>\$%&\|\\]/;
+
+  constructor(value: string) {
+    if (!value || value.trim().length === 0) {
+      throw new Error("SafeString cannot be empty or blank");
+    }
+
+    if (SafeString.dangerousCharactersPattern.test(value)) {
+      throw new Error("The string contains dangerous characters that may compromise security.");
+    }
+
+    this.value = value;
+  }
+
+  public getValue(): string {
+    return this.value;
+  }
+
+  public toString(): string {
+    return this.value;
+  }
+
+  public equals(other: SafeString): boolean {
+    return this.value === other.value;
+  }
+}
